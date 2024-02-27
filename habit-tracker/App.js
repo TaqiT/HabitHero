888aaaeaebfb9b662eb94855dbe551c1b0893d82
Evan Toomey <etoomey81@gmail.com>
2024-02-27 00:31:52 -0500

    can partially display tasks

--- habit-tracker/App.js
+++ habit-tracker/App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+class Task{
+  constructor(name, point_value){
+    this.name = name;
+    this.point_value = point_value;
+  }
+  get_name() {
+    return this.name;
+  }
+  get_point_value() {
+    return this.point_value;
+  }
+}
+
+const App = () => {
+  const task1 = new Task("Do the dishes", 10);
+  const task2 = new Task("Take out the trash", 5);
+  const taskList = [task1, task2];
+  return DisplayTaskList(taskList);
+}
+
+const DisplayTask = task => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#f00',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  return (
+    <View style={styles}>
+      <Text>{task.get_name()} - {task.get_point_value()} points</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const DisplayTaskList = (taskList) => {
   return (
     <View style={styles.container}>
-      <Text>Sample text</Text>
+      <DisplayTask task={taskList[0]} />
+      <DisplayTask task={taskList[0]} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +56,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
