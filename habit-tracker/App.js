f488a227330b8d951463c5ac307e7af18a93e71b
Evan Toomey <etoomey81@gmail.com>
2024-03-01 14:00:09 -0500

    formatting

--- habit-tracker/App.js
+++ habit-tracker/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 
+var task_list = [];
+
 class Task{
-  constructor(id, name, point_value){
-    this.id = id;
+  constructor(name, point_value){
+    this.id = task_list.length;
     this.name = name;
     this.point_value = point_value;
+    task_list.push(this);
   }
   get_name() {
     return this.name;
@@ -19,23 +22,22 @@ class Task{
 }
 
 const App = () => {
-  var task1 = new Task(0, "Do the dishes", 10);
-  var task2 = new Task(1, "Take out the trash", 5);
-  var taskList = [task1, task2];
-  return DisplayTaskList(taskList);
+  var task1 = new Task("Do the dishes", 10);
+  var task2 = new Task("Take out the trash", 5);
+  return DisplayTaskList(task_list);
 }
 
 const DisplayTaskList = (taskList) => {
   return (
     <View>
       {taskList.map((task, index) => (
-        <TouchableOpacity
+        <View
           key = {index}
           style = {styles.container}>
           <Text style={styles.text}>
             {task.get_task_str()}
           </Text>
-        </TouchableOpacity>
+        </View>
       ))}
       <StatusBar style="auto" />
     </View>
@@ -44,10 +46,14 @@ const DisplayTaskList = (taskList) => {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
+    marginHorizontal: 60,
     padding: 5,
+    width: 300,
     marginTop: 50,
-    backgroundColor: '#fff',
+    backgroundColor: '#A5A5A5',
     alignItems: 'center',
+    display: 'flex',
   },
   text: {
     color: '#000',
