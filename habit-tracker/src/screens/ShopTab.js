1d6bbbf7289917eaef2127c7ca69b4b5e13dbe05
Evan Toomey <etoomey81@gmail.com>
2024-03-07 20:49:14 -0500

    fixed spacing again

--- habit-tracker/src/screens/ShopTab.js
+++ habit-tracker/src/screens/ShopTab.js
@@ -23,25 +23,25 @@ class Task{
 }
 
 const ShopTab = () => {
-    var task1 = new Task('Task Name', 9999);
-    var task2 = new Task('Take out the trash', 5);
-    var task3 = new Task('Clean the bathroom', 15);
-    var task4 = new Task('Do the laundry', 20);
-    var task5 = new Task('Sweep the floors', 10);
-    var task6 = new Task('Mop the floors', 15);
-    var task7 = new Task('Clean the kitchen', 10);
-    var task8 = new Task('Clean the living room', 10);
-    var task9 = new Task('Clean the bedroom', 10);
-    var task10 = new Task('Clean the office', 10);
-    var task11 = new Task('Clean the garage', 10);
-    var task12 = new Task('Clean the car', 10);
-    var task13 = new Task('Clean the yard', 10);
-    var task14 = new Task('Clean the pool', 10);
-    var task15 = new Task('Clean the shed', 10);
-    var task16 = new Task('Clean the attic', 10);
-    return (
-        DisplayTaskList(task_list)
-    )
+  var task1 = new Task('Task Name', 9999);
+  var task2 = new Task('Take out the trash', 5);
+  var task3 = new Task('Clean the bathroom', 15);
+  var task4 = new Task('Do the laundry', 20);
+  var task5 = new Task('Sweep the floors', 10);
+  var task6 = new Task('Mop the floors', 15);
+  var task7 = new Task('Clean the kitchen', 10);
+  var task8 = new Task('Clean the living room', 10);
+  var task9 = new Task('Clean the bedroom', 10);
+  var task10 = new Task('Clean the office', 10);
+  var task11 = new Task('Clean the garage', 10);
+  var task12 = new Task('Clean the car', 10);
+  var task13 = new Task('Clean the yard', 10);
+  var task14 = new Task('Clean the pool', 10);
+  var task15 = new Task('Clean the shed', 10);
+  var task16 = new Task('Clean the attic', 10);
+  return (
+    DisplayTaskList(task_list)
+  )
 }
 
 
@@ -134,6 +134,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
 });
-
 
 export default ShopTab;
--- habit-tracker/src/screens/TaskTab.js
+++ habit-tracker/src/screens/TaskTab.js
@@ -17,25 +17,25 @@ class Task{
   }
 }
 
-  const HomeTab = () => {
-    var task2 = new Task('Take out the trash', 5);
-    var task3 = new Task('Clean the bathroom', 15);
-    var task4 = new Task('Do the laundry', 20);
-    var task5 = new Task('Sweep the floors', 10);
-    var task6 = new Task('Mop the floors', 15);
-    var task7 = new Task('Clean the kitchen', 10);
-    var task8 = new Task('Clean the living room', 10);
-    var task9 = new Task('Clean the bedroom', 10);
-    var task10 = new Task('Clean the office', 10);
-    var task11 = new Task('Clean the garage', 10);
-    var task12 = new Task('Clean the car', 10);
-    var task13 = new Task('Clean the yard', 10);
-    var task14 = new Task('Clean the pool', 10);
-    var task15 = new Task('Clean the shed', 10);
-    var task16 = new Task('Clean the attic', 10);
-    return (
-        DisplayTaskList(task_list)
-    )
+const HomeTab = () => {
+  var task2 = new Task('Take out the trash', 5);
+  var task3 = new Task('Clean the bathroom', 15);
+  var task4 = new Task('Do the laundry', 20);
+  var task5 = new Task('Sweep the floors', 10);
+  var task6 = new Task('Mop the floors', 15);
+  var task7 = new Task('Clean the kitchen', 10);
+  var task8 = new Task('Clean the living room', 10);
+  var task9 = new Task('Clean the bedroom', 10);
+  var task10 = new Task('Clean the office', 10);
+  var task11 = new Task('Clean the garage', 10);
+  var task12 = new Task('Clean the car', 10);
+  var task13 = new Task('Clean the yard', 10);
+  var task14 = new Task('Clean the pool', 10);
+  var task15 = new Task('Clean the shed', 10);
+  var task16 = new Task('Clean the attic', 10);
+  return (
+    DisplayTaskList(task_list)
+  )
 }
 
 
@@ -136,6 +136,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
 });
-
 
 export default HomeTab;
