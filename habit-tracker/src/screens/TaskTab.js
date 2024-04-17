6b27fd1cc1526a6abe6a5cfbfbdf0917adbf3ec5
Evan Toomey <etoomey81@gmail.com>
2024-04-17 15:58:53 -0400

    changed how task items are added

--- habit-tracker/src/screens/TaskTab.js
+++ habit-tracker/src/screens/TaskTab.js
@@ -1,38 +1,32 @@
 import React, { useState } from 'react'
 import {
-  StyleSheet, ScrollView, View, StatusBar, Modal, Text, TouchableOpacity, TextInput
+  StyleSheet, ScrollView, View, StatusBar, Modal, Text, TouchableOpacity, TextInput,
 } from 'react-native';
 import TaskComponent from '../components/Task';
 
-var task_list = [];
+var taskCount = 0;
 
 class Task {
   constructor(name, point_value) {
-    this.id = task_list.length;
+    this.id = taskCount++;
     this.name = name;
     this.point_value = point_value;
     this.completed = false;
-    task_list.push(this);
   }
 }
 
-const TaskTab = () => {
-  var task2 = new Task('Take out the trash', 9999);
-  var task3 = new Task('Clean the bathroom', 10);
-  var task4 = new Task('Do the laundry', 10);
-  var task5 = new Task('Sweep the floors', 10);
-  var task6 = new Task('Mop the floors', 10);
-  var task7 = new Task('Clean the kitchen', 10);
-  var task8 = new Task('Clean the living room', 10);
-  var task9 = new Task('Clean the bedroom', 10);
-  var task10 = new Task('Clean the office', 10);
-  var task11 = new Task('Clean the garage', 10);
-  var task12 = new Task('Clean the car', 10);
-  var task13 = new Task('Clean the yard', 10);
-  var task14 = new Task('Clean the pool', 10);
-  var task15 = new Task('Clean the shed', 10);
-  var task16 = new Task('Clean the attic', 10);
+var task_list = [
+  new Task('Take out the trash', 9999),
+  new Task('Clean the bathroom', 10),
+  new Task('Do the laundry', 10),
+  new Task('Sweep the floors', 10),
+  new Task('Mop the floors', 10),
+  new Task('Clean the kitchen', 10),
+  new Task('Clean the living room', 10),
+  new Task('Clean the bedroom', 10),
+];
 
+const TaskTab = () => {
   return (
     DisplayTaskList(task_list)
   );
@@ -66,7 +60,11 @@ const DisplayTaskList = (taskList) => {
             <View style={{ height: 550, width: 350 }} />
             <TouchableOpacity
               style={[styles.doneButton, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                newTask = new Task('Clean the attic', 10);
+                taskList.push(newTask);
+                }}>
               <Text style={styles.textStyle}>Done</Text>
             </TouchableOpacity>
           </View>
